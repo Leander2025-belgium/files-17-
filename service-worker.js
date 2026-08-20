c2bf682ff36df102155e8fcfe7c44203b642c63a
Leander2025-belgium <Devriendtleander2@gmail.com>
2026-08-20 11:18:43 +0200

    Restore compact home icon controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "wheaterflow-v20260820-tv-route-1";
+const CACHE_VERSION = "wheaterflow-v20260820-home-icons-1";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const STATIC_ASSETS = [
   "./",
