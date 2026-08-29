cbc6a3c24dd6092bbd425d62a1d8a6a15e972146
Leander2025-belgium <Devriendtleander2@gmail.com>
2026-08-29 23:25:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "wheaterflow-v20260829-liquid-glass-v5-clear";
+const CACHE_VERSION = "wheaterflow-v20260829-nav-icons-31";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const STATIC_ASSETS = [
   "./",
