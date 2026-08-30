7083300d338e2803b445cca5c4493c5a9fba2cb1
Leander2025-belgium <Devriendtleander2@gmail.com>
2026-08-30 18:41:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "wheaterflow-v20260830-photo-composer-v2";
+const CACHE_VERSION = "wheaterflow-v20260830-photo-composer-v3-mobile-fit";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const STATIC_ASSETS = [
   "./",
