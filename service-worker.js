77d3050a27d746c6bd789300a7137c40a1202678
Leander2025-belgium <Devriendtleander2@gmail.com>
2026-09-04 22:16:30 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "wheaterflow-v20260904-rain-bars-fix";
+const CACHE_VERSION = "wheaterflow-v20260904-radar-contrast-v1";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const STATIC_ASSETS = [
   "./",
