1d4e9b94b974b83968a2f825fa21dfd4be38c11d
Leander2025-belgium <Devriendtleander2@gmail.com>
2026-09-08 19:32:41 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "wheaterflow-v20260908-laptop-v1";
+const CACHE_VERSION = "wheaterflow-v20260908-profile-searchfix-v1";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const STATIC_ASSETS = [
   "./",
